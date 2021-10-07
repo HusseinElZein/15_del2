363d72ddaf30fa4aab74bc50dbe309df54069f37
Yusuf Kara <yusufkarasan@hotmail.com>
2021-10-07 15:09:35 +0200

    Vi har tilføjet lidt kode til terning, og oprettet en Main klasse.

--- src/Spil/Terning.java
+++ src/Spil/Terning.java
@@ -1,4 +1,20 @@
 package Spil;
 
 public class Terning {
+
+    private int terning, terningSum;
+
+    public void Terning() {
+
+        int terningSum = 0;
+
+        int terning = (int) (Math.random() * 11 + 2);
+
+        terningSum += terning;
+
+        if (terningSum > 12) {
+            terning = terningSum - 12;
+            terningSum = terningSum - 12;
+        }
+    }
 }
