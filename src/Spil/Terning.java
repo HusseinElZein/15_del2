34386e8eddd95c22b7195d9d6dae4efdb83d6e62
Yusuf Kara <yusufkarasan@hotmail.com>
2021-10-07 18:58:54 +0200

    Sidste commit på dagen. Vi har lavet et spil, som fungerer.

--- src/Spil/Terning.java
+++ src/Spil/Terning.java
@@ -2,19 +2,41 @@ package Spil;
 
 public class Terning {
 
-    private int terning, terningSum;
+    private int terning;
+    private static int terningSum1,terningSum2;
 
-    public void Terning() {
+    public String Terning1() {
+        this.terning = (int) (Math.random()*11+2);
+        terningSum1+=terning;
 
-        int terningSum = 0;
-
-        int terning = (int) (Math.random() * 11 + 2);
-
-        terningSum += terning;
-
-        if (terningSum > 12) {
-            terning = terningSum - 12;
-            terningSum = terningSum - 12;
+        if(terningSum1 > 12) {
+            terningSum1 = terningSum1 - 12;
         }
+
+        return "har slået "+ terning + " \nDu står på felt "+ terningSum1;
+    }
+
+    public static int getTerningSum1() {
+        return terningSum1;
+    }
+
+    public String Terning2() {
+        this.terning = (int) (Math.random()*11+2);
+        terningSum2+=terning;
+
+        if(terningSum2 > 12) {
+            terningSum2 = terningSum2 - 12;
+        }
+
+        return "har slået "+ terning + " \nDu står på felt "+ terningSum2;
+    }
+
+    public static int getTerningSum2() {
+        return terningSum2;
+    }
+
+    public int getTerning() {
+        return terning;
     }
 }
+
