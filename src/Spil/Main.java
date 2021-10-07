5dbfe1652545ef9c7662446e42b7be5e1d58fbb0
Yusuf Kara <yusufkarasan@hotmail.com>
2021-10-07 15:44:07 +0200

    Tilføjet Runde klassen

--- src/Spil/Main.java
+++ src/Spil/Main.java
@@ -8,11 +8,20 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Velkommen til feltspillet!\n" +
+                "Tryk enter for at starte spillet: ");
+
+        String enter = sc.nextLine();
+
+        System.out.println();
+
         System.out.print("Spiller 1, indtast dit navn: ");
         String navn1 = sc.nextLine();
 
         System.out.print("Spiller 2, indtast dit navn: ");
         String navn2 = sc.nextLine();
+
+        System.out.println();
 
         Spiller sp1 = new Spiller(navn1);
         Spiller sp2 = new Spiller(navn2);
